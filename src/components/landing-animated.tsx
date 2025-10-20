5a6ce4cef9ff6fe5d350d45f3f117c5d03ebe65f
Madiyar Iznalfiden <madiyar.iznalfiden@gmail.com>
2025-10-20 14:49:22 +0500

    sagve

--- src/components/landing-animated.tsx
+++ src/components/landing-animated.tsx
@@ -11,7 +11,7 @@ import MainPageProductsOverviewFlow from './MainPageProductsOverviewFlow';
 
 
 export default function LandingAnimated() {
-  
+
   const partners: { name: string; src: string }[] = [
     { name: 'Alstom', src: '/Alstom_logo.png' },
     { name: 'Allur', src: '/Allur_logo_red_(1).png' },
